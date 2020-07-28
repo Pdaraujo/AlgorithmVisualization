f9a47ee4ba25e23c54fdff7ee062215a84068a89
diogoaraujo <diogo.araujo@twisper.com>
2020-07-28 15:10:20 +0100

    added logic for merge sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
-import "./SortingVisualizer.css"
-import "../SortingAlgorithms/SortingAlgorithms"
-import { getMergeSort } from '../SortingAlgorithms/SortingAlgorithms';
+import "./SortingVisualizer.css";
+import { getMergeSort, getMergeSortAnimations } from '../SortingAlgorithms/SortingAlgorithms';
 
 // Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 1;
@@ -40,7 +39,27 @@ export default class SortingVisualizer extends Component {
     }
 
     mergeSort = () => {
-
+        const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+        const arrayBars = document.getElementsByClassName('array-bar');
+        const isColorChange = i % 3 !== 2;
+        if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+            setTimeout(() => {
+            barOneStyle.backgroundColor = color;
+            barTwoStyle.backgroundColor = color;
+            }, i * ANIMATION_SPEED_MS);
+        } else {
+            setTimeout(() => {
+            const [barOneIdx, newHeight] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            barOneStyle.height = `${newHeight}px`;
+            }, i * ANIMATION_SPEED_MS);
+        }
+    }
         
     }
 
